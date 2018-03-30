0481fb3a5de403ec299dee86b8888c30276f53c8
Santeri94 <santeri.liukkonen94@gmail.com>
2018-03-30 14:12:13 +0200

    Added nodemon for quicker development workflow. Added descriptions to DB elements. Also added a new route to show a campsite.

--- app.js
+++ app.js
@@ -9,32 +9,78 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("styles"));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://www.oregonhikers.org/w/images/e/ec/Mt._Hood%2C_Salmon_Creek_Greenway.jpg"},
-  {name: "Pirkkolan Leirialue", image: "https://mw2.google.com/mw-panoramio/photos/medium/2652538.jpg"},
-  {name: "Virojoen Kirkonkylä", image: "http://3bv8x43y68hc448rg43goku7yq-wpengine.netdna-ssl.com/media/2012/01/Virojoki-Road_06-990x659.jpg"}
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Syntetic creation of an element on startup if needed:
+
+/*  Campground.create({
+    name: "Pirkkolan Leirialue",
+    image: "https://mw2.google.com/mw-panoramio/photos/medium/2652538.jpg",
+    description: "This is a nice campground in Finland. Especially good for children."
+  }, function(err, campground){
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Newly created campground: ");
+      console.log("campground");
+    }
+  });
+*/
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
-
-
-  res.render("campgrounds",{campgrounds:campgrounds});
+  // Get all campgrounds from DB
+  Campground.find({}, function (err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("index",{campgrounds:allCampgrounds});
+    }
+  });
 });
 
+// POST - create a new campground
 app.post("/campgrounds", (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image}
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc}
+  // Create a new campground and sace to DB
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", (req, res) => {
   res.render("new.ejs");
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if(err) {
+      console.log(err);
+
+    } else {
+      res.render("show", {campground: foundCampground});
+    }
+  });
 });
 
 app.listen(3000, process.env.IP, function() {
