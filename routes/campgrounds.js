ff287792ce09cee9c3295bc5da2e120051326614
Santeri94 <santeri.liukkonen94@gmail.com>
2018-06-18 21:23:33 +0200

    Finalized rudimental dynamic pricing addition to individual items

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,13 +18,14 @@ router.get("/", function(req, res) {
 // POST - create a new campground
 router.post("/", middleware.isLoggedIn, (req, res) => {
   var name = req.body.name;
+  var price = req.body.price;
   var image = req.body.image;
   var desc = req.body.description;
   var author = {
     id: req.user._id,
     username: req.user.username
   }
-  var newCampground = {name: name, image: image, description: desc, author: author}
+  var newCampground = {name: name, price: price, image: image, description: desc, author: author}
 
   // Create a new campground and sace to DB
   Campground.create(newCampground, function(err, newlyCreated) {
